f3275ea16c9835d669e572fbd05d5edf3762bee2
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-20 21:35:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function volume_sphere() {
+function volume_sphere(event) {
+  event.preventDefault(); // Add this line to prevent form submission
+
   const radius = document.getElementById("radius").value;
-    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-    document.getElementById("volume").value = volume.toFixed(2);
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  document.getElementById("volume").value = volume.toFixed(2);
 }
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+  document.getElementById('MyForm').addEventListener('submit', volume_sphere);
+}
