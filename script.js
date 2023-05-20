f326c2b3b76a5346298b09d845dd4ae4d8c17353
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-20 21:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  const radius = document.getElementById("radius").value;
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById("volume").value = volume.toFixed(2);
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
